1ff14a4155a648962f8a3aeabfc191d5217dff7d
Robson Vaamonde <robsonvaamonde@gmail.com>
2025-08-14 20:11:17 -0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 3307
+  port: Number(process.env.DB_PORT) || 3306
 })
 
 connection.connect(err => {
